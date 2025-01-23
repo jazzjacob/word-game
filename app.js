8ae1ab47452e52248068193e23e6d1715b45b685
Jacob Reinikainen Lindström <80760206+jazzjacob@users.noreply.github.com>
2025-01-23 18:58:43 +0100

    kind of alphaguess functionality

--- app.js
+++ app.js
@@ -6,11 +6,78 @@ button.addEventListener('click', onButtonClick);
 const textInput = document.getElementById("word");
 const messageBox =  document.createElement("div");
 const body = document.querySelector('body');
+const guessedWordsBeforeElement = document.createElement("ul");
+const guessedWordsAfterElement = document.createElement("ul");
+
+const wordOfTheDay = "hell";
+const formElement = document.querySelector('form');
+const wordOfTheDayElement = document.createElement("p");
+wordOfTheDayElement.innerHTML = `Today's word: ${wordOfTheDay}`;
+
+body.insertBefore(wordOfTheDayElement, formElement);
+
+const guessedWordsBefore = [];
+const guessedWordsAfter = [];
+
+function compareWithTodaysWord(word) {
+  console.log("Comparing with todays word!");
+  console.log(`${word} vs ${wordOfTheDay}`);
+
+  if (word == wordOfTheDay) {
+    console.log("WORD OF THE DAY FOUND!");
+    return;
+  }
+
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] == wordOfTheDay[i]) {
+      console.log(`${word[i]} == ${wordOfTheDay[i]}`);
+    } else if (word[i] < wordOfTheDay[i]) {
+      // Word comes before word of the day alphabetically
+      console.log(`${word[i]} < ${wordOfTheDay[i]}`);
+      console.log(`"${word}" comes before "${wordOfTheDay}" alphabetically.`);
+      addGuessedWord(word, "before");
+      return;
+    } else {
+      // Word comes before after of the day alphabetically
+      console.log(`${word[i]} > ${wordOfTheDay[i]}`);
+      console.log(`"${word}" comes after "${wordOfTheDay}" alphabetically.`);
+      addGuessedWord(word, "after");
+      return;
+    }
+  }
+  // The word contains the same letters as the word of the day, but is shorter
+  // E.g. word="hell", but word of the day is "hello"
+  console.log(`"${word}" comes before "${wordOfTheDay}" alphabetically.`);
+  addGuessedWord(word, "before");
+  return;
+}
+
+function addGuessedWord(word, placement) {
+  const wordListItem = document.createElement("li");
+
+  if (placement == "before") {
+    guessedWordsBefore.push(word);
+    wordListItem.innerHTML = word;
+    guessedWordsBeforeElement.appendChild(wordListItem);
+    body.insertBefore(guessedWordsBeforeElement, formElement);
+
+  } else if (placement == "after") {
+    guessedWordsAfter.push(word);
+    wordListItem.innerHTML = word;
+    guessedWordsAfterElement.appendChild(wordListItem);
+    body.insertBefore(guessedWordsAfterElement, formElement.nextSibling);
+
+  } else {
+    console.error("Erroneous useage of placement argument.");
+  }
+
+  console.log(guessedWordsBefore);
+  console.log(guessedWordsAfter);
+}
 
 function formatText(text) {
   return text.toLowerCase();
 }
-
 
 function onButtonClick(event) {
   event.preventDefault();
@@ -87,6 +154,7 @@ function app(word) {
       if (i === word.length - 1) {
         if (currentObjectIsAWord) {
           printSuccessMessage(word);
+          compareWithTodaysWord(word);
         } else {
           printNoWordFoundMessage(word);
         }
